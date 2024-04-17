5328b1f5aa2fcec4104123b07d5a928c9bc958ff
Samilly <samillynunes19@gmail.com>
2024-04-17 20:00:57 -0300

    Working with routes

--- src/App.js
+++ src/App.js
@@ -1,48 +1,9 @@
-import React, { useEffect, useState } from "react";
-import './App.css';
+import RoutesApp from "./routes";
 
 function App(){
 
-  const [nutri, setNutri] = useState([]);
-
-  // Quando o array de observacao esta vazio, carrega quando monta
-  useEffect(()=>{
-
-    function loadApi(){
-      let url = 'https://sujeitoprogramador.com/rn-api/?api=posts';
-      
-      fetch(url)
-      .then((result)=>result.json())
-      .then((json) => {
-        console.log(json);
-        setNutri(json);
-      });
-    }
-
-    loadApi();
-  }, []);
-
   return (
-    <div className="container">
-      <header>
-        <strong>React Nutri</strong>
-      </header>
-
-      {nutri.map(item => {
-        return (
-          <article key={item.id} className="post">
-            <strong className="title">{item.titulo}</strong>
-            <img src={item.capa} alt={item.title} />
-            <p className="subtitle">
-              {item.subtitulo}
-            </p>
-            <a className="accessBtn" > Acessar</a>
-
-          </article>
-        )
-      })}
-
-    </div>
+    <RoutesApp/>
   );
 }
 
